2e77bf7fe1cc6297ab37c423181e9fc03e2d0e6e
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-21 00:51:03 +0600

    Create 12-4.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE II/FILE MANAGEMENT IN C/Easyway/12-4.c
@@ -0,0 +1,28 @@
+// append two file [12.4]
+
+#include <stdio.h>
+
+int main()
+{
+    FILE *source = fopen("source.txt", "a");
+    FILE *dest = fopen("dest.txt", "r");
+
+    if (source == NULL || dest == NULL)
+    {
+        printf("Error files open or file missing!\n");
+        return 1;
+    }
+
+    int a;
+    while ((a = fgetc(dest)) != EOF)
+    {
+        fputc(a, source);
+    }
+
+    printf("\nFile append successfully!\n\n");
+
+    fclose(source);
+    fclose(dest);
+
+    return 0;
+}
