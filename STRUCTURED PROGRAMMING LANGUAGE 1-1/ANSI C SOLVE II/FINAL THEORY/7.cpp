36f90f9deb7b9fb947074756a1783cc549aa1bc1
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-21 00:51:50 +0600

    Create 7.cpp

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE II/FINAL THEORY/7.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl "\n"
+int f(int a)
+{
+    return a * a;
+}
+int main()
+{
+    int a = 5;
+    double x = f(a);
+    cout << x << nl;
+
+    return 0;
+}
