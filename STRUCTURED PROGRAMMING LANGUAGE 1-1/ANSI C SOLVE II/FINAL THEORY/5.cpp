710f0379397f629c286cf0e4d7952d1b84fe08f1
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-21 00:51:48 +0600

    Create 5.cpp

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE II/FINAL THEORY/5.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl "\n"
+void func(string a,int s)
+{
+    cout<<a<<nl;
+}
+int main()
+{
+   func("prince");
+
+return 0;
+}
