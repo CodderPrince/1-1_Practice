82e17f71c2b5700e352dcdb7fd2ab24bb5343d3b
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-21 00:55:11 +0600

    Create 11-10.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE II/POINTERS/FINAL SUBMIT/11-10.c
@@ -0,0 +1,55 @@
+/*old 11.10 new 12.10 [PRINCE]
+Write a function (using pointer parameters) that compares two integer arrays
+to see whether they are identical. The function returns 1 if they are identical,
+0 otherwise.
+*/
+
+#include <stdio.h>
+
+int check(int *arr1, int *arr2, int size);
+
+int main()
+{
+    int size;
+    printf("\nEnter the size of the arrays: ");
+    scanf("%d", &size);
+
+    int arr1[size];
+    printf("\nEnter the elements of the first array: ");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr1[i]);
+    }
+
+    int arr2[size];
+    printf("\nEnter the elements of the second array: ");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr2[i]);
+    }
+
+    int result = check(arr1, arr2, size);
+
+    if (result)
+    {
+        printf("\nArrays are identical.\n\n");
+    }
+    else
+    {
+        printf("\nArrays are not identical.\n\n");
+    }
+
+    return 0;
+}
+
+int check(int *arr1, int *arr2, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if (*(arr1 + i) != *(arr2 + i))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
