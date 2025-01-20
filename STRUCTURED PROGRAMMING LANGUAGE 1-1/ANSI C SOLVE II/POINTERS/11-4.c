180faf8af9915451ba1a5e2962ba14086237bdc9
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-21 00:55:03 +0600

    Create 11-4.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE II/POINTERS/11-4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void addMatrices(int *mat1, int *mat2, int rows, int cols, int resMat[rows][cols]);
+
+int main()
+{
+    
+    int mat1[] = {1, 2, 3,
+                  4, 5, 6};
+    int mat2[] = {3, 4, 5,
+                  1, 2, 3};
+
+    int rows = 2;
+    int cols = 3;
+
+    int resMat[rows][cols];  
+
+    addMatrices(mat1, mat2, rows, cols, resMat);
+    printf("\nAfter adding two matrices:\n");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%5d", resMat[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    return 0;
+}
+
+void addMatrices(int *mat1, int *mat2, int rows, int cols, int resMat[rows][cols])
+{
+    int i, j;
+    for (i = 0; i < rows; i++)
+    {
+        for (j = 0; j < cols; j++)
+        {
+            resMat[i][j] = *(mat1 + i * cols + j) + *(mat2 + i * cols + j);
+        }
+    }
+}
