f1e74339a4b9926287669724378437ada17bfd70
Md. An Nahian Prince <annahian44@gmail.com>
2025-01-21 00:51:52 +0600

    Create 9.cpp

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE II/FINAL THEORY/9.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl "\n"
+int x = 10;
+void f()
+{
+    int x = 10 + 5;
+    cout << x << endl;
+}
+int main()
+{
+    cout << x << endl;
+    f();
+    cout << x << endl;
+    return 0;
+}
