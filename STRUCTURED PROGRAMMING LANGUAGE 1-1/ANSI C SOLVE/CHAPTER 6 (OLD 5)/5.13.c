490f819a186ad7e507c5cbbb712cd82d0ee5a5c5
Md. An Nahian Prince <annahian44@gmail.com>
2025-04-29 23:57:38 +0600

    Create 5.13.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE/CHAPTER 6 (OLD 5)/5.13.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,count=0,sum=0;
+    for(i=0;i<=100;i++)
+    {
+        if((i%6==0)&&(i%4!=0))
+        {
+            sum=sum+i;
+            count++;
+            printf("%d  ",i);
+        }
+
+
+    }
+    printf("\n\n");
+    printf("count: %d\n",count);
+    printf("Summation: %d\n",sum);
+
+    return 0;
+}
