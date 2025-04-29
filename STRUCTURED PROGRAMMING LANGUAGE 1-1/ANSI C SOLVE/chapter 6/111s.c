3bfe64d110e46a869941b24ce6ad08638e1c5123
Md. An Nahian Prince <annahian44@gmail.com>
2025-04-29 23:58:18 +0600

    Create 111s.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE/chapter 6/111s.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() 
+{
+    
+   int m;
+   for(m=1; m<10; m= m+1)
+   {
+    printf("%d",m)
+   }
+   while(m<10)
+   {
+
+   }
+
+    return 0;
+}
+
+#include <stdio.h>
+
+int main() 
+{
+    int m = 1;
+    do 
+    {
+        printf("%d", m);
+        m = m + 1;
+    } 
+    while (m < 10);
+
+    return 0;
+}
+
