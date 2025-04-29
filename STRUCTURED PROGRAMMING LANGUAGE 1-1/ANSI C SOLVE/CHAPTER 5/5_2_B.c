b043a458000b421a1263c0e252a38fe801ca05ff
Md. An Nahian Prince <annahian44@gmail.com>
2025-04-29 23:57:32 +0600

    Create 5_2_B.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE/CHAPTER 5/5_2_B.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float b,x,y;
+    printf("Enter the value of x & y: ");
+    scanf("%f %f",&x,&y);
+        b = (x-y)/2;
+        printf("(b)= %.3f",b);
+    
+    return 0;
+}
