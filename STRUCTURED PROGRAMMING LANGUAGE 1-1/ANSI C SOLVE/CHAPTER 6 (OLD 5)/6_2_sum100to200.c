e1dc5c7c97ec1e627e99e7dbf97d71b258d1c0c6
Md. An Nahian Prince <annahian44@gmail.com>
2025-04-29 23:58:04 +0600

    Create 6_2_sum100to200.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE/CHAPTER 6 (OLD 5)/6_2_sum100to200.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    int sum=0;
+    for (int i=101; i<200; i++)
+     {
+        x= i%7;
+        if(x==0)
+        {
+            sum= sum+i;
+        }
+     }
+     printf("Sum: %d\n",sum);
+       return 0;
+    }
