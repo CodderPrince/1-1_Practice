aa54df24bb053401e9d2f55cda9d302ab4fe6d8d
Md. An Nahian Prince <annahian44@gmail.com>
2025-04-30 00:04:11 +0600

    Create 8.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE/MID-2 SOLVE & PRACTICE/8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() 
+{
+    int count = 1275;
+    double price = 235.74;
+    printf("%d\n",5%7);
+    printf("%d\n",15%(int)2.5);
+    const char city[] = "Cambridge";
+
+    printf("%-10d %-15s Hello Mohona!\n", count, city);
+
+    return 0;
+}
