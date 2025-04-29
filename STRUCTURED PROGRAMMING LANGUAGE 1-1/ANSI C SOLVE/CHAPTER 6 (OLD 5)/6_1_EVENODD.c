6c94772c249782e6f0c22fdf314ac1077fe38d75
Md. An Nahian Prince <annahian44@gmail.com>
2025-04-29 23:57:58 +0600

    Create 6_1_EVENODD.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE/CHAPTER 6 (OLD 5)/6_1_EVENODD.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("Enter an integer number: ");
+    scanf("%d",&x);
+    if(x%2==0)
+    {
+        printf("NUMBER IS EVEN\n");
+    }
+    else\
+    {
+         printf("NUMBER IS ODD\n");
+    }
+ return 0;
+}
