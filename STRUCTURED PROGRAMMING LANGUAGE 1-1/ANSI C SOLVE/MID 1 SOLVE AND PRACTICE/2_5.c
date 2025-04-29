d678f166ce1d2c413ac47fb4450c991766911cac
Md. An Nahian Prince <annahian44@gmail.com>
2025-04-30 00:03:18 +0600

    Create 2_5.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE/MID 1 SOLVE AND PRACTICE/2_5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float rice=16.75, sugar=15.00;
+    printf("***LIST OF ITEMS***\n\n");
+    printf("Item\tPrice\n");
+    printf("Rice\tRs %.2f\n",rice);
+    printf("Sugar\tRs %.2f\n",sugar);
+
+    return 0;
+}
