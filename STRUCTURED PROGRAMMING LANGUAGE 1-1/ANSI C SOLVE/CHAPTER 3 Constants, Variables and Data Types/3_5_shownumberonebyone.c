e5f36370c4f3c7bf0bc018d70b24b98cd22682fd
Md. An Nahian Prince <annahian44@gmail.com>
2025-04-29 23:56:50 +0600

    Create 3_5_shownumberonebyone.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE/CHAPTER 3 Constants, Variables and Data Types/3_5_shownumberonebyone.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    char a[101];
+    int b,i;
+    printf("Enter a number: ");
+    scanf("%s",a);
+    for(i=0; i<strlen(a); i++)
+    {
+        int a%10
+    }
+}
