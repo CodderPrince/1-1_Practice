77a49917f70ec9de5d0dbefe91f1da45561634f2
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-02 02:29:30 +0600

    Create 15charactercountWA.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE/SUBEEN 52 PROBLEM SOLUTION/SolveWith C/15charactercountWA.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[10001];//jehetu string size deya ase ar null character er jonno 1 besi niyesi
+        int i, count[26]={0};//alphabet 26 ta tai array size toto niyesi
+        gets(str);
+        //for(i=0; i<26; i++)
+        //{//jehetu ekhane count korte hobe tai eng 26 ta character kei null kore diyesi
+            //count[i]=0;
+       // }
+        for(i=0; i<=strlen(str); i++)
+        {//ebar word ta count korbo tai tar length porjonto loop use korbo
+            if(str[i]>= 'a' && str[i]<= 'z')
+            {//etar explain niche exp soho dibo
+                count[str[i]-'a']++;
+            }//etar explain oo niche
+        }
+        for(i=0; i<26; i++)
+        {
+            if(count[i]!=0)
+            {
+                printf("%c = %d\n", 'a'+i, count[i]);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+/* prince
+ if(str[i]>= 'a' && str[i]<= 'z')
+ p ki 'a' theke boro or soman and z theke soto or soman ..true           
+                count[str[i]-'a']++;
+ 0 index e p ase tai p er ascii value 112-97= 15  
+ count variable er moddhe 15 index e p ase tai tar value 1 increase korbe
+ abar r= 113-97=16 index a r ase tai count er 16 index er value 1 increase korbe
+ eibhabe serially cholte thakbe        
+*/
