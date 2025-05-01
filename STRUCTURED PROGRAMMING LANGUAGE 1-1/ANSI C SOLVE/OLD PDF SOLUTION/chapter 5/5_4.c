7dbd3f524d1fb029c46d39569b5f52d9eb2f963c
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-02 02:28:58 +0600

    Create 5_4.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE/OLD PDF SOLUTION/chapter 5/5_4.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int number;
+    if(number>=80 && number<=100)
+    {
+        printf("More than 80 marks");
+    }
+    else if(number>=60 && number<80)
+    {
+        printf("More than 60 marks")
+    }
+    else if(number>=40 && number<60)
+    {
+        printf("More than 40 marks")
+    }
+    else if(number>=60 && number<80)
+    {
+        printf("More than 60 marks")
+    }
+}
