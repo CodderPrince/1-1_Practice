3bbc5b3a9a077db471b3af1eebc224c3cd718ffb
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-01 19:51:30 +0600

    Create 1_1.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/ANSI C SOLVE/OLD PDF SOLUTION/1_1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("First line\t:Md.An Nahian Prince\n");
+    printf("Second line\t:Door No,Street\n");
+    printf("Third line\t:City,Pin code\ntgyhuijkopl]|
+    ");
+}
