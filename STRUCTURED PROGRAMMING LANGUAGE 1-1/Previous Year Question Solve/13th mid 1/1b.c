69eaecfc2d452c5dfd8cbfc39daec42d0e510ad3
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-03 00:26:28 +0600

    Create 1b.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/Previous Year Question Solve/13th mid 1/1b.c
@@ -0,0 +1,39 @@
+/*
+convert this code in while loop
+for(a=1; a<=100; a++)
+printf("%d\n",a*a)
+*/
+
+/*
+#include<stdio.h>
+int main()
+{
+    int a=1;
+    while(a<=100)
+    {
+        printf("%d\n",a*a);
+        a++;
+    }
+}
+*/
+
+//convert above code in do while loop
+
+#include<stdio.h>
+int main()
+{
+    int a=1;
+    do
+    {
+        printf("%d\n",a*a);
+        a++;
+    }
+    while(a<=100);
+    printf("\n");
+}
+/*
+do ..... while
+er statement holo emon do loop er modde ese 1st e print kora start korbe
+then increment or decrement hobe and while loop er moddhe only condition hobe
+practice more and more to solve this type of problem easily
+*/
