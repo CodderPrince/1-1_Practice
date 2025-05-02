68f937fa8c1c8177de9087bad0789d436dc5c0ee
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-03 00:26:33 +0600

    Create 3b.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/Previous Year Question Solve/2017-18 1st year 2nd/3b.c
@@ -0,0 +1,13 @@
+//consider the following code snippet
+//snippet means consider small wrong things like semicolon
+#include<stdio.h>
+int main()
+{
+int a=9;
+a= a>>2;
+printf("%d\n",a);
+}
+//output result is 2 because here we input like cpp
+
+
+//what should be the output for this code snippet?//
