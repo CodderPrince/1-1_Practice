0343ecd6e8466296d0890c51696a298dd7b34970
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-04 19:08:48 +0600

    Create 7recOptimize.cpp

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SEMESTER 2 SOLVE/50DP/OPTIMIZE/7recOptimize.cpp
@@ -0,0 +1,50 @@
+/*
+DP PROBLEM NUMBER : 7
+SHORTEST SUPERSEQUENCE LENGTH[sslL]
+Name: PRINCE
+ID: 12105007
+Algorithm: Recursion
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl endl
+
+int dp[1001][1001];
+
+int ssl(string s1, int m, string s2, int n)
+{
+
+    // base case
+    if (m == 0 || n == 0)
+    {
+        return dp[m][n] = m + n;
+    }
+    if (dp[m][n] != -1)
+    {
+        return dp[m][n];
+    }
+    if (s1[m - 1] == s2[n - 1])
+    {
+        return dp[m][n] = 1 + ssl(s1, m - 1, s2, n - 1);
+    }
+    else
+    {
+        return dp[m][n] = min(1 + ssl(s1, m - 1, s2, n), 1 + ssl(s2, m, s2, n - 1));
+    }
+}
+
+int main()
+{
+    string s1 = "bat", s2 = "cat";
+    int m = s1.length(), n = s2.length();
+
+    memset(dp, -1, sizeof(dp));
+
+    int x = ssl(s1, m, s2, n);
+    cout << "Sortest Supersequence: " << x << nl;
+
+    return 0;
+}
