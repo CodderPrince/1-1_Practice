8b2133f07a8d2eb6429d23b7b6c0f45bcdf7399d
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-05 20:00:13 +0600

    Create 77.cpp

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SEMESTER 2 SOLVE/50DP/PRACTICE/77.cpp
@@ -0,0 +1,69 @@
+/*
+DP PROBLEM NUMBER : 7
+SHORTEST SUPERSEQUENCE LENGTH
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int SCSLength(string X, string Y)
+{
+    int m = X.length();
+    int n = Y.length();
+
+    // Create a 2D vector array of size (m+1) x (n+1)
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    // Fill in the base cases
+    for (int i = 0; i <= m; i++)
+    {
+        dp[i][0] = i;
+    }
+    for (int j = 0; j <= n; j++)
+    {
+        dp[0][j] = j;
+    }
+
+    // Fill in the rest of the elements
+    for (int i = 1; i <= m; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (X[i - 1] == Y[j - 1])
+            {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else
+            {
+                dp[i][j] = min(dp[i][j - 1] + 1, dp[i - 1][j] + 1);
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+int main()
+{
+    string X = "ABCBDAB", Y = "BDCABA";
+
+    cout << "The length of the shortest common supersequence is "
+         << SCSLength(X, Y);
+
+    return 0;
+}
+
+/*
+AFTER COMPLETE BASE CASE 2D VECTOR LOOK LIKE
+    0   1   2   3
+    1   0   0   0
+    2   0   0   0
+    3   0   0   0
+
+AFTER COMPLETE ALL ITERATIONS FINAL OUTPUT: DP[3][3] = 4
+
+
+TIME COMPLEXITY: O(M*N)
+WHICH IS MORE FASTER THAN RECURSION CALL: O(2^(M+N))
+*/
