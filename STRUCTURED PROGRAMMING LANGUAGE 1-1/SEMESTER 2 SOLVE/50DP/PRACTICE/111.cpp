3a24002ea07e81232633672627e1f074f382030e
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-05 19:59:10 +0600

    Create 111.cpp

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SEMESTER 2 SOLVE/50DP/PRACTICE/111.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007            // 1e9 + 7
+#define PI 3.14159265358979323846 // acos(-1)
+#define nl "\n"
+int main()
+{
+    char *ptr;
+    char mystring[] = "abcdefg";
+    ptr = mystring;
+    ptr += 5;
+    cout << *ptr;
+
+    return 0;
+}
