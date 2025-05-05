a3b8eb48882162a4aae42dbeb3412e959afd381f
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-05 20:02:16 +0600

    Create 9-8.cpp

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SEMESTER 2 SOLVE/ALL SUBIT/CH-9 SUBMIT/9-8.cpp
@@ -0,0 +1,32 @@
+// convert lowercase to uppercase
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl endl
+
+void convert(string x)
+{
+    for (ll i = 0; i < x.size(); i++)
+    {
+        if (x[i] >= 'a' && x[i] <= 'z')
+        {
+            x[i] -= 32;
+        }
+        else if (x[i] >= 'A' && x[i] <= 'Z')
+        {
+            x[i] = x[i];
+        }
+    }
+    cout << x << nl; 
+}
+int main()
+{
+    cout << "Given a lowercase string to: ";
+    string str;
+    getline(cin, str);
+    convert(str);
+
+    return 0;
+}
