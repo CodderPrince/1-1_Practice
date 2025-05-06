293c24bd14965abd7ef2e84fe90fe7c0ec7239f3
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-07 01:09:20 +0600

    Create 8.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/Lab Final Practice/8.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main()
+{
+    int t,i;
+    scanf("%d",&t);
+    //while(t--)
+    for(i=1; i<=t; i++)
+    {
+        int a,b,c,temp;
+        scanf("%d %d %d",&a,&b,&c);
+        if(a>b)
+        {
+            temp=a;
+            a=b;
+            b=temp;            
+        }
+        if(a>c)
+        {
+            temp=a;
+            a=c;
+            c=temp;
+        }
+        if(b>c)
+        {
+            temp=b;
+            b=c;
+            c=temp;
+        }
+        printf("Case %d: %d %d %d\n",i,a,b,c);
+    }
+    
+    return 0;
+}
