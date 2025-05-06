c986acd2cecb4f001109d51d1baddaa4d5d5b409
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-07 01:09:49 +0600

    Create 14kdldd.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/14kdldd.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[101],ch[2];
+        int count=0;
+        gets(str);
+        gets(ch);
+        int i;
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]==ch[0])
+            {
+               count++;
+            }
+            
+        }
+        if(count>0)
+        {
+            printf("Occurence %c in %s is = %d\n",ch[0],str,count);
+        }
+        else
+        {
+            printf("'%c' is not present\n",ch[0]);
+        }
+    }
+
+    return 0;
+}
