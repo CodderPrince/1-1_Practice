f0811641ac799bf75564f470c77dbfe4018589c8
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-06 18:30:57 +0600

    Create 9-11.cpp

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SEMESTER 2 SOLVE/ANSI DEBUGGING/FUNCTION/9-11.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl "\n"
+int xyz()
+{
+    return 5;
+}
+int main()
+{
+    int x = xyz() + xyz();
+    cout << x << nl;
+
+    return 0;
+}
