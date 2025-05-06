4dc7be95e6129ce987620dc7ae2e4ecfb137bdf4
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-06 18:31:24 +0600

    Create 10-8a.cpp

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SEMESTER 2 SOLVE/ANSI DEBUGGING/STRUCT AND UNION/10-8a.cpp
@@ -0,0 +1,37 @@
+//template
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl "\n"
+
+/*template <typename T>
+T add(T a, T b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int x = 5, y = 10;
+    cout << add(x, y) << endl; // Output: 10
+
+    double a = 3.14, b = 2.71;
+    cout << add(a, b) << endl; // Output: 3.14
+
+    return 0;
+}*/
+
+template <typename t>
+t add(t a, t b)
+{
+    return a + b;
+}
+int main()
+{
+    int x = 10, y = 5;
+    cout << add(x, y) << nl;
+    float m = 1.2, n = 2.3;
+    cout << add(m, n) << nl;
+}
