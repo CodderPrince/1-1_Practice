ac152e86597da83a6e9e933acedc2fbf885dba61
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-06 18:31:32 +0600

    Create tempCodeRunnerFile.cpp

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SEMESTER 2 SOLVE/ANSI DEBUGGING/STRUCT AND UNION/tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nl "\n"
+
+int main()
+{
+    int arr[] = {2, 3, 1};
+    cout << "Array size: " << sizeof(arr) << nl;
+
+    return 0;
+}
