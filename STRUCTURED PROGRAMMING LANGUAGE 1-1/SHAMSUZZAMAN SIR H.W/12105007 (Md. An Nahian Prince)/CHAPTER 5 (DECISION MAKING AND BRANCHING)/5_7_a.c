c682d3da55d0b6827c8352a137f7a62836083b0f
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-07 01:08:05 +0600

    Create 5_7_a.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/12105007 (Md. An Nahian Prince)/CHAPTER 5 (DECISION MAKING AND BRANCHING)/5_7_a.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int row, count=0;
+    printf("\nEnter row number: ");
+    scanf("%d",&row);
+    for(int i=1; i<=row; i++)
+    {
+        for(int j=1; j<=i; j++)
+        {
+            count++;
+            printf("%d",count);
+            printf(" ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
+    return 0;
+}
