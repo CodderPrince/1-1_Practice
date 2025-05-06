0930d9b389cef9641bf9b83896b531ad6bc81155
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-07 01:07:59 +0600

    Create 10-44.cpp

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SEMESTER 2 SOLVE/STRUCTURE AND UNIONS/PRACTICE/10-44.cpp
@@ -0,0 +1,31 @@
+#include <bits\stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define nl endl
+
+struct date
+{
+    ll day;
+    ll month;
+    ll year;
+};
+
+int main()
+{
+    struct date Date;
+
+    cout << "Enter day: ";
+    cin >> Date.day;
+
+    cout << "Enter month: ";
+    cin >> Date.month;
+
+    cout << "Enter year: ";
+    cin >> Date.year;
+
+    // Do something with the entered date here
+
+    return 0;
+}
