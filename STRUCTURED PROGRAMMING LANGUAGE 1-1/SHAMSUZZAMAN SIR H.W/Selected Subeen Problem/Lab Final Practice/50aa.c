be7d5394d6e2df32c00ad64fb58aa182e0e515ab
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-07 01:09:17 +0600

    Create 50aa.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/Lab Final Practice/50aa.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[101];
+        gets(str);
+        int i;
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]=='L')
+            {
+                str[i]=str[i-1];
+            }
+            else if(str[i]=='R')
+            {
+                str[i]=str[i+1];
+            }
+            else
+            {
+                str[i]=str[i];
+            }
+        }
+        printf("%s\n",str);
+    }
+
+    return 0;
+}
