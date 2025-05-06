25ba9da475187c9b25133073c01a91ab12213d54
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-07 01:09:34 +0600

    Create 12.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/12.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,count=0,i;
+        scanf("%d",&n);
+        for(i=5; i<=n; i*=5)
+        {
+            count= count + n/i;
+        }
+        printf("%d\n",count);
+    }
+    return 0;
+}
