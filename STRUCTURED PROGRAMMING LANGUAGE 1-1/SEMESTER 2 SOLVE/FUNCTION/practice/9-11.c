96ed6109e4566895fce77e2f89509ec7756e42cc
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-07 01:07:11 +0600

    Create 9-11.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SEMESTER 2 SOLVE/FUNCTION/practice/9-11.c
@@ -0,0 +1,42 @@
+// old 9.11 new 10.11
+// input [m][n] matrix and find the largest value
+
+#include <stdio.h>
+
+int largest(int m, int n, int matrix[m][n])
+{
+    int max = matrix[0][0];
+    int i, j;
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (max < matrix[i][j])
+            {
+                max = matrix[i][j];
+            }
+        }
+    }
+    printf("Largest value of the matrix is: %d\n", max);
+}
+
+int main()
+{
+    int m, n;
+    printf("Enter the row size: ");
+    scanf("%d", &m);
+    printf("Enter the coloumn size: ");
+    scanf("%d", &n);
+    int matrix[m][n], i, j, max;
+    printf("Enter the matrix element\n");
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    largest(m, n, matrix);
+
+    return 0;
+}
