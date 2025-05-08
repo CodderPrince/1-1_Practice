c52eb5e544bcbc82082ae9f5ff1a1b8ae922477a
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-08 19:59:46 +0600

    Create 15kdlll.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/15kdlll.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[101];
+        gets(str);
+        int count[26]={0},i;
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]>='a' && str[i]<='z')
+            {
+                count[str[i]-'a']++;
+            }
+        }
+        for(i=0; i<26; i++)
+        {
+            if(count[i]!=0)
+            {
+                printf("%c = %d\n",'a'+i,count[i]);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
