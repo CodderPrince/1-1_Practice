cec488fd6a48d521d2cf6d2cccd84d7e07455c7a
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:02:17 +0600

    Create 18kdkdld.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/18kdkdld.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[101];
+        gets(str);
+        int i,count=0;
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')
+            {
+                printf("%c",str[i]);
+            }
+        }
+        printf("\n");
+
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]!='a' && str[i]!='e' && str[i]!='i' && str[i]!='o' && str[i]!='u' && str[i]!=' ')
+            {
+                printf("%c",str[i]);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
