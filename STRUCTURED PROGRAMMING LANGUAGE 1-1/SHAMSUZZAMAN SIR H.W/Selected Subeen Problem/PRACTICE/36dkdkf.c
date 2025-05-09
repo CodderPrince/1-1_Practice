aaf6bfb38447ce1825eb8aca65e4105b8ec1ebd4
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:03:03 +0600

    Create 36dkdkf.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/36dkdkf.c
@@ -0,0 +1,43 @@
+//word sorting
+
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        char str[n][101];
+        int i;
+        for(i=0; i<n; i++)
+        {
+            scanf("%s",str[i]);
+        }
+        int j;
+        for(i=0; i<n; i++)
+        {
+            for(j=i+1; j<n; j++)
+            {
+                if(strcmp(str[i],str[j])>0)
+                {
+                    char temp[101];
+                    strcpy(temp,str[i]);
+                    strcpy(str[i],str[j]);
+                    strcpy(str[j],temp);
+                }
+            }
+        }
+        for(i=0; i<n; i++)
+        {
+            printf("%s\n",str[i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+}
