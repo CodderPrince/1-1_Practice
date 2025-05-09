377fd558d43b0b3ea34df2b99dd31d3221a391cb
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:02:32 +0600

    Create 23fkdfkd.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/23fkdfkd.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[101];
+        gets(str);
+        int i;
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]>='A' && str[i]<='Z')
+            {
+                printf("%d",str[i]-64);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
