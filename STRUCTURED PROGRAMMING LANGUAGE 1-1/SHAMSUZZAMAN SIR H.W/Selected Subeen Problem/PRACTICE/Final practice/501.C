37e7d7c7305747b0a1a2305c9b553817c1a34855
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-10 00:24:23 +0600

    Create 501.C

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/Final practice/501.C
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    getchar();
+    while (t--) {
+        int i;
+        char str1[101], str2[101];
+        gets(str1);
+
+        for (i = 0; i < strlen(str1); i++) {
+            if (str1[i] == 'L') {
+                str2[i] = str1[i - 1];
+            } else if (str1[i] == 'R') {
+                str2[i] = str1[i + 1];
+            } else {
+                str2[i] = str1[i];
+            }
+        }
+
+        str2[strlen(str1)] = '\0';
+
+        // Replace all '9' with '4'
+        for (i = 0; i < strlen(str2); i++) {
+            if (str2[i] == '9') {
+                str2[i] = '4';
+            }
+        }
+
+        // Print the resulting string
+        printf("%s\n", str2);
+    }
+
+    return 0;
+}
