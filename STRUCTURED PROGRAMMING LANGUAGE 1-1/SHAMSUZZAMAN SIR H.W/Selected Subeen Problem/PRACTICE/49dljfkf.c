9e55d4430a3da839c13dc71f2cbd0f5bd1dff7c2
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:05:08 +0600

    Create 49dljfkf.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/49dljfkf.c
@@ -0,0 +1,32 @@
+#include<stdio.h.>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        unsigned long long int n;
+        int i;
+        scanf("%llu",&n);
+        for(i=2; i*i<=n; i++)
+        {
+            if(n==1)
+            {
+                continue;
+            }
+            if(n%i==0)
+            {
+                printf("%llu is not a prime\n",n);
+                break;
+            }
+        }
+
+        if(i*i>n)
+        {
+            printf("%llu is a prime\n",n);
+        }
+    }
+
+    return 0;
+}
