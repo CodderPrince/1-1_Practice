5b3fdb7c3fa59add875d5493d3113f3888ffa5af
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:05:22 +0600

    Create 7kdldlf.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/7kdldlf.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[101];
+        gets(str);
+        int i,count=1;
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i] == ' ')
+            {
+                count++;
+            }
+        }
+        printf("%d\n",count);
+    }
+
+    return 0;
+}
