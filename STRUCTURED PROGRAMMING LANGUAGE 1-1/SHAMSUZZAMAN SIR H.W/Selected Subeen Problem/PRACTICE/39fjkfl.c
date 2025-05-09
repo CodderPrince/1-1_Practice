afb48b15bd967866ce4274c9dd0f56363e2ec773
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:03:25 +0600

    Create 39fjkfl.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/39fjkfl.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[101], re[101];
+        gets(str);
+        strcpy(re,str);
+        strrev(re);
+        if(strcmp(str,re)==0)
+        {
+            printf("Yes! It is a palindrome!\n");
+        }
+        else
+        {
+            printf("Sorry! It is not palindrome!\n");
+        }
+    }
+
+    return 0;
+}
