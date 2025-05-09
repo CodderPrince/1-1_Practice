0e149ef12f47c37c85122ae65b176adc4b8b4bcf
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-10 00:24:59 +0600

    Create 166.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/FP 52p/166.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str1[1001],str2[1001];
+        gets(str1);
+        int i,j=0;
+        for(i=0; i<strlen)
+    }
+}
