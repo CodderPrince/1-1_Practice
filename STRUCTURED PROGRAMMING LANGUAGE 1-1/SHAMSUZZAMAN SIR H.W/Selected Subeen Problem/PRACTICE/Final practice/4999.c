9e59741b90e9c031cee4e5670182ebedd5442d50
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-10 00:24:20 +0600

    Create 4999.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/Final practice/4999.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+	int T,n,i,j,flag=0;
+	
+	scanf("%d", &T);
+	while(T--)
+	{
+		flag=0;
+		
+	scanf("%d", &n);
+
+		for(i=2;i<=n/2;i++){
+			if(n%i==0){
+				flag++;
+				break;
+			}
+			else flag=0;
+		}
+		if(flag==0 || n==2)
+		
+		printf("%d is a prime\n",n);
+		else
+		
+		printf("%d is not a prime\n",n);
+	}
+
+    return 0;
+}
