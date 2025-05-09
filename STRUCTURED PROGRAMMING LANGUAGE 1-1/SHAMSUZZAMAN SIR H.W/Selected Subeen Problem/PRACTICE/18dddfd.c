17ac8d4a8f933c6e0ca5b04e4cd93b707fa36c92
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:02:15 +0600

    Create 18dddfd.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/18dddfd.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        int i;
+        char str[10001];
+        gets(str);
+        for(i=0; i<strlen(str); i++)
+        {
+           if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')
+           {
+            printf("%c",str[i]);
+           }
+        }
+        printf("\n");
+        
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]!='a' && str[i]!='e' && str[i]!='i' && str[i]!='o' && str[i]!='u' && str[i]!=' ')
+            {
+                printf("%c",str[i]);
+            }
+        }
+        printf("\n");
+
+    }
+    return 0;
+}
