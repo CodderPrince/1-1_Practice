a038bdf0d64960c5f4d1f0f9645ba4bc70709b77
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:05:04 +0600

    Create 48ff.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/48ff.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n,i;
+        scanf("%d",&n);
+        int a[n-1];
+        for( i=0 ; i<(n-1) ; i++)
+        {
+            scanf( "%d",&a[i]);
+        }
+        int j,count;
+        for(i=1 ; i<=n ; i++)
+        {
+            count=0;
+            for( j=0 ; j<(n-1) ; j++)
+            {
+                if(i==a[j])
+                   {
+                      count=1;
+                        break;
+                   }
+            }
+            if(count==0)
+            {
+                printf("%d",i);
+            }
+        }
+    }
+    return 0;
+}
