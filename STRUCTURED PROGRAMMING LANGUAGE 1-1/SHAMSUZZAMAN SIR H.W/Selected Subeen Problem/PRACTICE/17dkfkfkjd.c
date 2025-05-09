ce7e9aa2ab3496c1296fa8930dddb20ef72928ed
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:02:12 +0600

    Create 17dkfkfkjd.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/17dkfkfkjd.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[101];
+        gets(str);
+        int i,count=0;
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')
+            {
+                count++;
+            }
+        }
+        printf("Number of vowels = %d\n",count);
+    }
+
+    return 0;
+}
