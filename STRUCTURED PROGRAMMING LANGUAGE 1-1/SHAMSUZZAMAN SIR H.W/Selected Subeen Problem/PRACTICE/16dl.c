d17b426ee11bd79091d048490c8703e4ae42b9a8
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:02:04 +0600

    Create 16dl.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/16dl.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        int i,j=0;
+        char str[101],count
+    }
+}
