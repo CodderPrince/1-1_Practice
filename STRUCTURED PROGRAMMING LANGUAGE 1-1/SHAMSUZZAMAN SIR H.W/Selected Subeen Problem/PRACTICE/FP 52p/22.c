17633e8ed88807723580efe3c0655346e7e41432
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-10 00:25:09 +0600

    Create 22.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/FP 52p/22.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int first,last,i,count=0,j;
+        scanf("%d %d",&first,&last);
+        for(i=first; i<=last; i++)
+        {
+            if(i==1)
+            {
+                continue;
+            }
+            for(j=2; j*j<=i; j++)
+            {
+                if(i%j==0)
+                {
+                    break;
+                }
+            }
+            if(j*j>i)
+            {
+                count++;
+            }
+        }
+        printf("%d\n",count);
+    }
+
+    return 0;
+}
