70ac3e08dbbba836e340d7da25ba289bc9a5f8a9
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:03:22 +0600

    Create 399hj.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/399hj.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[101],rev[101];
+        gets(str);
+        strcpy(rev,str);
+        strrev(rev);
+        if(strcmp(str,rev)==0)
+        {
+            printf("Yes! palindrome\n");
+        }
+        else
+        {
+            printf("NO");
+        }
+    }
+}
