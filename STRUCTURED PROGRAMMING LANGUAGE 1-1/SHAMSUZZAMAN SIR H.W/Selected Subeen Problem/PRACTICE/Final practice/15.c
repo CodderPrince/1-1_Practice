07efabdbe799c521a8d6c295640e8b68fa35a443
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:05:31 +0600

    Create 15.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/Final practice/15.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        int i, count[26]={0};
+        char str[101];
+        gets(str);
+
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]>='a' && str[i]<='z')
+            {
+                count[str[i]-'a']++;
+            }
+        }
+
+        for(i=0; i<26; i++)
+        {
+            if(count[i]!=0)
+            {
+                printf("%c = %d\n",'a'+i,count[i]);
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
