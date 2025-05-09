0b22aa68b4c80512a07959fa8cb6c81dd4240233
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:05:40 +0600

    Create 221.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/Final practice/221.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int first,last,count=0;
+        scanf("%d %d",&first,&last);
+        int i,j;
+        for(i=first; i<=last; i++)
+        {
+            if(i==1)
+            {
+                continue;
+            }
+
+            for(j=2; j*j<=i; j++)
+            {
+                if(i%j==0)
+                {
+                    break;
+                }
+            }
+
+            if(j*j>i)
+            {
+                count++;
+            }
+        }
+        printf("%d\n",count);
+    }
+
+    return 0;
+}
