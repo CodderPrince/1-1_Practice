aef28f1255a7edf43e72a2f9c285b025b03939f3
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:02:33 +0600

    Create 23ssdd.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/23ssdd.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+ 
+
+ int main()
+ {
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        int i;
+        char str[101];
+        gets(str);
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]>='A' && str[i]<='Z')
+            {
+                printf("%d",str[i]-64);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+ }
