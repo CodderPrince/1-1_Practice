9d137d2a7a70d0fe39555fbdf70ea973dc0726f6
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:02:01 +0600

    Create 16dddd.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/16dddd.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        int i,j=0;
+        char str1[1001],str2[1001];
+        gets(str1);//this is
+        for(i=0; i<strlen(str1); i++)
+        {
+            if(str1[i]!=' ')
+            {
+                str2[j] = str1[i];
+                j++;
+            }
+
+            else if(j>0)
+            {
+                str2[j] = '\0';
+                strrev(str2);
+                printf("%s ",str2);
+                j=0;
+            }
+
+
+            else
+            {
+                printf(" ");
+            }
+        }
+
+        if(j>0)
+        {
+            str2[j] = '\0';
+            strrev(str2);
+            printf("%s",str2);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
