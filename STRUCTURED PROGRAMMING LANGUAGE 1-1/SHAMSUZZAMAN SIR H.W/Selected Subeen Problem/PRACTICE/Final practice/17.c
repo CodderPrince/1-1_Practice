ed1b7f5fa0a508e0f1e61829240af4b3ce2fe91b
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:05:34 +0600

    Create 17.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/Final practice/17.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        int count=0,i;
+        char str[101];
+        gets(str);
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u')
+            {
+                count++;
+            }
+        }
+        printf("Number of vowels = %d\n",count);
+    }
+
+    return 0;
+}
