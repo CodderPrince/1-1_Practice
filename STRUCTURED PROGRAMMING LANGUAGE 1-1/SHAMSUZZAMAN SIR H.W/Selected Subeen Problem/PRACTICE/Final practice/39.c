759bc99718067c0a7ddc0302d3e9ae873896537d
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:05:57 +0600

    Create 39.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/Final practice/39.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str [101];
+        gets(str);
+        char str2 [101];
+        strcpy(str2,str);
+        strrev(str2);
+       if( strcmp(str,str2)==0)
+       {
+        printf("Yes! It is palindrome!\n");
+       }
+       else
+       {
+        printf("Sorry! It is not palindrome!\n");
+       }
+    }
+
+    return 0;
+}
