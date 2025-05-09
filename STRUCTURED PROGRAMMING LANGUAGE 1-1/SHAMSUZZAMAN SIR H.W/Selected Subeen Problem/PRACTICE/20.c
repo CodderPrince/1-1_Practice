20301b52c8b246b95db0b6e5f7c3b2c10666e02f
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-09 12:02:20 +0600

    Create 20.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/20.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[10001];
+        int i,count=0;
+        gets(str);
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]== ' ' && str[i+1]!= ' ')
+            {
+                count ++;
+            }
+        }
+        printf("Count = %d\n",count+1);
+    }
+    return 0;
+}
