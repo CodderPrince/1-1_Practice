97f4a528dbcaf89cf5b672bf3c8f646f87a13b86
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-11 00:04:30 +0600

    Create 38dd.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/FP 52p/38dd.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int row,col,x,y;
+        scanf("%d %d",&x,&y);
+
+        for(row=0; row<x; row++)
+        {
+            for(col=0; col<=row; col++)
+            {
+                printf("%d ",y);
+            }
+            printf("\n");
+        }
+
+        for(row= x-1; row>0; row--)
+        {
+            for(col=0; col<row; col++)
+            {
+                printf("%d ",y);
+            }
+            printf("\n");
+        }
+       printf("\n");
+    }
+
+    return 0;
+}
