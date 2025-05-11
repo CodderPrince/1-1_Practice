09afb4854a96ec651e5999754cfc95f0cf8a3c44
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-11 10:00:12 +0600

    Create 14.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SHAMSUZZAMAN SIR H.W/Selected Subeen Problem/PRACTICE/lab practice/14.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[101],ch[2];
+        gets(str);
+        gets(ch);
+        int i,count=0;
+        for(i=0; i<strlen(str); i++)
+        {
+            if(str[i]==ch[0])
+            {
+                count++;
+            }
+        }
+        if(count>0)
+        {
+            printf("Occurence of '%c' in '%s' = %d\n",ch[0],str,count);
+        }
+        else
+        {
+            printf("'%c' is not present\n",ch[0]);
+        }
+    }
+
+    return 0;
+}
