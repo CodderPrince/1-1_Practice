c8e32b557ce8723536b1f9aefc83c17165dec3bc
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-13 01:03:43 +0600

    Create 31perfectnumber2.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/SUBEEN 52 PROBLEM SOLUTION/SolveWith C/31perfectnumber2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        int ara[5] = {6, 28, 496, 8128, 33550336};
+        int i, n;
+        scanf("%d", &n);
+        for(i = 0; i < 5; i++)
+        {
+            if(ara[i] <= n)
+            {
+                printf("%d\n", ara[i]);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    return 0;
+}
