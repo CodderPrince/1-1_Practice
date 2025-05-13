bd1106cd3c38ee4fe3fb757e0f068a421ac57715
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-13 09:10:57 +0600

    Create 23.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/Subeen selected problem practice/23.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    getchar();
+    while(t--)
+    {
+        char str[15];
+        int i;
+        gets(str);
+        for(i=0; i<strlen(str); i++)
+        {
+
+            if(str[i]>='A' && str[i]<='Z')
+            {
+                printf("%d",str[i]-64);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
