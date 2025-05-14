8cfa3e42ae667097008296ea09bddc4969dd0426
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-15 00:04:47 +0600

    Create P25 LCM 1[TIME LIMIT EXCEEDED].c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/Subeen selected problem practice/Subeen 52 problem solution/P25 LCM 1[TIME LIMIT EXCEEDED].c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+    int n1, n2, lcm,i;
+    scanf("%d %d", &n1, &n2);
+    if(n1>n2)
+    {
+        lcm = n1;
+    }
+    else
+    {
+        lcm = n2;
+    }
+    for(i=0; i<t; i++)
+    {
+
+
+        if( lcm%n1 == 0 && lcm%n2 == 0)
+        {
+        printf("LCM = %d\n", lcm);
+        break;
+        }
+        lcm++;
+
+    }
+    }
+
+
+return 0;
+}
