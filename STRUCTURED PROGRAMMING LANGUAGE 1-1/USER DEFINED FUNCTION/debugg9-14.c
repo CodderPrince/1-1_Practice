b7da5d8744577e179ad0c4a0c6c0922e2074d573
Md. An Nahian Prince <annahian44@gmail.com>
2025-05-15 00:05:21 +0600

    Create debugg9-14.c

---
+++ STRUCTURED PROGRAMMING LANGUAGE 1-1/USER DEFINED FUNCTION/debugg9-14.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int prod(int m, int n);
+int main()
+{
+    int x=10,y=20;
+    int p,q;
+    p = prod(x,y);
+    q = prod(p, prod(x,y));
+    printf("%d %d\n",p,q);
+}
+int prod(int a, int b)
+{
+    return (a*b);
+}
